b6998c006ba82dc7325bbc5f0f5ef62c542e47f3
cutecatsandvirtualmachines <145232977+cutecatsandvirtualmachines@users.noreply.github.com>
2024-11-26 18:48:32 +0100

    fix apt install

--- companionsetup.sh
+++ companionsetup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt update
 sudo apt install -y build-essential meson ninja-build pkg-config \
-                diffutils
+                diffutils \
                 python3 python3-venv  \
                 libglib2.0-dev libusb-1.0-0-dev libncursesw5-dev \
                 libpixman-1-dev libepoxy-dev libv4l-dev libpng-dev \
